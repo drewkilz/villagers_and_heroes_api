594d6533bd15e5c902d6cc24f580c1f0688673a7
Drew Kilz <drew@thespottedlynx.com>
2020-06-07 14:18:16 -0600

    Test the application on Heroku, spitting out data from the calculations.

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
+from math import trunc
+
 from flask import Flask
+
+from db import Data
+from models.item import SALVAGE_KIT
+from models.recipes import Recipes
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    recipes = (("Golem's Firm Axe", 40),)
+    data = Data()
+    recipes_ = Recipes(data)
+    for recipe_name, quantity in recipes:
+        recipes_.add(data.get_recipe(recipe_name), quantity)
+    recipes_.calculate(salvaging=True)
+
+    value = 'Raw Materials<br />'
+    for name, (item, quantity) in recipes_.items.items():
+        if name == SALVAGE_KIT:
+            stacks = quantity / 30.0
+        else:
+            stacks = quantity / item.stack_size
+        value += '\t{0: >5}   {1:>5.2}   {2}<br />'.format(quantity, stacks, name)
+
+    value += '<br />'
+    value += 'Crafted Materials<br />'
+    for name, (recipe, quantity) in recipes_.crafted.items():
+        value += '\t{0: >5}   {1:>5.2}   {2}<br />'.format(quantity, quantity / recipes_.data.get_item(recipe.name).stack_size, name)
+
+    value += '<br />'
+    value += 'Recipes<br />'
+    for recipe, quantity in recipes_.list:
+        value += '\t{0: >5}   {1}<br />'.format(quantity, recipe.name)
+
+    value += '<br />'
+    value += 'Gold cost<br />'
+    gold = trunc(recipes_.cost)
+    silver = trunc(recipes_.cost * 100 - gold * 100)
+    copper = trunc(recipes_.cost * 10000 - gold * 10000 - silver * 100)
+    value += '\t{} gp\t{} sp\t{} cp'.format(gold, silver, copper)
+
+    return value
 
 
 if __name__ == '__main__':
--- models/recipes.py
+++ models/recipes.py
@@ -8,30 +8,30 @@ from models.recipe import Recipe
 
 
 class Recipes(Object):
-    _list: List[Tuple[Recipe, int]]
-    _crafted: Dict[str, Tuple[Recipe, int]]
-    _items: Dict[str, Tuple[Item, int]]
+    list: List[Tuple[Recipe, int]]
+    crafted: Dict[str, Tuple[Recipe, int]]
+    items: Dict[str, Tuple[Item, int]]
 
     def __init__(self, data: Data):
-        self._list = []
-        self._crafted = {}
-        self._items = {}
-        self._cost = 0
-        self._data = data
+        self.list = []
+        self.crafted = {}
+        self.items = {}
+        self.cost = 0
+        self.data = data
 
     def add(self, recipe: Recipe, quantity: int):
-        self._list.append((recipe, quantity))
+        self.list.append((recipe, quantity))
 
     def _calculate(self, recipe: Recipe, quantity: int, salvaging=False):
         quantity_ = quantity
 
         # Add salvage kits, as needed
         if salvaging and recipe.type.is_salvageable():
-            if self._data.salvage_kit.name in self._items:
-                self._items[self._data.salvage_kit.name] = (self._data.salvage_kit, quantity +
-                                                            self._items[self._data.salvage_kit.name][1])
+            if self.data.salvage_kit.name in self.items:
+                self.items[self.data.salvage_kit.name] = (self.data.salvage_kit, quantity +
+                                                          self.items[self.data.salvage_kit.name][1])
             else:
-                self._items[self._data.salvage_kit.name] = (self._data.salvage_kit, quantity)
+                self.items[self.data.salvage_kit.name] = (self.data.salvage_kit, quantity)
 
         for ingredient in recipe.ingredients:
             if salvaging and recipe.type.is_salvageable():
@@ -42,34 +42,34 @@ class Recipes(Object):
                     # Roughly 65% are returned, so can reduce by that amount
                     quantity_ = ceil(quantity * 0.35)
 
-            sub_recipe = self._data.get_recipe(ingredient.item.name)
+            sub_recipe = self.data.get_recipe(ingredient.item.name)
             if sub_recipe:
                 self._calculate(sub_recipe, quantity_ * ingredient.quantity, salvaging=salvaging)
-                if sub_recipe.name in self._crafted:
-                    self._crafted[sub_recipe.name] = (sub_recipe, quantity_ * ingredient.quantity +
-                                                      self._crafted[sub_recipe.name][1])
+                if sub_recipe.name in self.crafted:
+                    self.crafted[sub_recipe.name] = (sub_recipe, quantity_ * ingredient.quantity +
+                                                     self.crafted[sub_recipe.name][1])
                 else:
-                    self._crafted[sub_recipe.name] = (sub_recipe, quantity_ * ingredient.quantity)
+                    self.crafted[sub_recipe.name] = (sub_recipe, quantity_ * ingredient.quantity)
             else:
-                if ingredient.item.name in self._items:
-                    self._items[ingredient.item.name] = (ingredient.item, quantity_ * ingredient.quantity +
-                                                         self._items[ingredient.item.name][1])
+                if ingredient.item.name in self.items:
+                    self.items[ingredient.item.name] = (ingredient.item, quantity_ * ingredient.quantity +
+                                                        self.items[ingredient.item.name][1])
                 else:
-                    self._items[ingredient.item.name] = (ingredient.item, quantity_ * ingredient.quantity)
+                    self.items[ingredient.item.name] = (ingredient.item, quantity_ * ingredient.quantity)
 
-        self._cost += (recipe.cost * quantity)
+        self.cost += (recipe.cost * quantity)
 
     def calculate(self, salvaging=False):
-        self._crafted = {}
-        self._items = {}
-        self._cost = 0
+        self.crafted = {}
+        self.items = {}
+        self.cost = 0
 
-        for recipe, quantity in self._list:
+        for recipe, quantity in self.list:
             self._calculate(recipe, quantity, salvaging=salvaging)
 
     def print(self):
         print('Raw Materials')
-        for name, (item, quantity) in self._items.items():
+        for name, (item, quantity) in self.items.items():
             if name == SALVAGE_KIT:
                 stacks = quantity / 30.0
             else:
@@ -78,17 +78,17 @@ class Recipes(Object):
 
         print()
         print('Crafted Materials')
-        for name, (recipe, quantity) in self._crafted.items():
-            print('\t{0: >5}   {1:>5.2}   {2}'.format(quantity, quantity / self._data.get_item(recipe.name).stack_size, name))
+        for name, (recipe, quantity) in self.crafted.items():
+            print('\t{0: >5}   {1:>5.2}   {2}'.format(quantity, quantity / self.data.get_item(recipe.name).stack_size, name))
 
         print()
         print('Recipes')
-        for recipe, quantity in self._list:
+        for recipe, quantity in self.list:
             print('\t{0: >5}   {1}'.format(quantity, recipe.name))
 
         print()
         print('Gold cost')
-        gold = trunc(self._cost)
-        silver = trunc(self._cost * 100 - gold * 100)
-        copper = trunc(self._cost * 10000 - gold * 10000 - silver * 100)
+        gold = trunc(self.cost)
+        silver = trunc(self.cost * 100 - gold * 100)
+        copper = trunc(self.cost * 10000 - gold * 10000 - silver * 100)
         print('\t{} gp\t{} sp\t{} cp'.format(gold, silver, copper))
