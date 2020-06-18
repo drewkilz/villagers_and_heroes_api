c6237d6d1ddd6e597cc1b81877a5c412a61cadfe
Drew Kilz <drew@thespottedlynx.com>
2020-06-18 13:32:10 -0600

    Add two new potential tools and a TODO file.

---
+++ todo.py
@@ -0,0 +1,12 @@
+"""
+Simply a temporary file containing ideas of what to work on next.
+"""
+
+# TODO: react.js to actually do something!!
+# TODO: Pagination on the recipes page
+# TODO: Implement user registration and login as it will be needed for village maintenance
+# TODO: Implement village tool - import screenshots (roster or village project), parse, then store and allow display
+#  over time of contributions, etc. - for this one, may need a new backend database, so split out into a separate api
+#  application, maybe
+# TODO: Implement crafting party tool - list out all the enhancements to crafting speed to allow checking, then
+#  calculate the number of resources needed per 20 minute crafting buff
