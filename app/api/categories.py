ef9af4f408f20fb0b071208a4afea9e862f1a981
Drew Kilz <drew@thespottedlynx.com>
2020-06-20 04:39:16 -0600

    Add a new route for skills (e.g. Crafting Skills vs Gathering Skills vs Village Skills).

--- app/api/categories.py
+++ app/api/categories.py
@@ -1,8 +1,12 @@
 from flask import jsonify, abort
+from sqlalchemy_filters import apply_filters
 
 from app.api import api
 from app.models.category import Category
+from app.models.enum import CraftingSkill, GatheringSkill, VillageSkill
 from app.models.schemas.category import CategorySchema
+from app.models.schemas.type import TypeSchema
+from app.models.type import Type
 
 
 @api.route('/categories/<str_or_int:id_or_name>')
@@ -26,4 +30,33 @@ def get_categories():
 
     return jsonify({
         'categories': CategorySchema(many=True).dump(categories)
+    })
+
+
+@api.route('/skills/<name>')
+def get_skills(name):
+    enum = None
+    if name == 'Crafting':
+        enum = CraftingSkill
+    elif name == 'Gathering':
+        enum = GatheringSkill
+    elif name == 'Village':
+        enum = VillageSkill
+    else:
+        abort(404)
+
+    skill_names = []
+
+    for skill in enum:
+        skill_names.append(skill.value)
+
+    filter_spec = [
+        {'field': 'category_id', 'op': 'eq', 'value': Category.query.filter_by(name='Skill').one().id},
+        {'field': 'name', 'op': 'in', 'value': skill_names},
+    ]
+
+    types = apply_filters(Type.query, filter_spec).all()
+
+    return jsonify({
+        'types': TypeSchema(many=True).dump(types)
     })
