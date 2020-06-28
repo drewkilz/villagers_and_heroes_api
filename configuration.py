6dda1ccd10be2f21c92c82399eea29e3bfccd786
Drew Kilz <drew@thespottedlynx.com>
2020-06-28 15:54:30 -0600

    Change CORS origin environment variable to be either a string or a list of strings.

--- configuration.py
+++ configuration.py
@@ -23,7 +23,7 @@ class Configuration:
     SQLALCHEMY_TRACK_MODIFICATIONS: str = os.environ.get(ENV_SQLALCHEMY_TRACK_MODIFICATIONS) or False
     VNH_ITEMS_PER_PAGE: int = 20
     VNH_RECIPES_PER_PAGE: int = 20
-    CORS_ORIGIN: str = os.environ.get(ENV_CORS_ORIGIN)
+    CORS_ORIGIN: list = []
 
     def init_app(self, app: Flask):
         if not self.SECRET_KEY:
@@ -33,6 +33,13 @@ class Configuration:
         if not self.SQLALCHEMY_DATABASE_URI:
             raise EnvironmentError('No {} configured in environment variables for Flask-SQLAlchemy'.format(
                 ENV_SQLALCHEMY_DATABASE_URI))
+
+        cors_origin = os.environ.get(ENV_CORS_ORIGIN)
+        if cors_origin:
+            try:
+                self.CORS_ORIGIN = eval(cors_origin)
+            except SyntaxError as e:
+                raise EnvironmentError('Error in configuration of {}: {}'.format(ENV_CORS_ORIGIN, e))
 
 
 class DevelopmentConfiguration(Configuration):
