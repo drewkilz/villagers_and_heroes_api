bf8d598d0e684c098c443a5a67206f5abc1e3193
Drew Kilz <drew@thespottedlynx.com>
2020-08-02 19:56:27 -0600

    Remove the TODO file as they were placed into the Issues tracker on GitHub.

--- todo.py
+++
@@ -1,8 +0,0 @@
-"""
-Simply a temporary file containing ideas of what to work on next.
-"""
-
-# TODO: Implement user registration and login as it will be needed for village maintenance
-# TODO: Implement village tool - import screenshots (roster or village project), parse, then store and allow display
-#  over time of contributions, etc. - for this one, may need a new backend database, so split out into a separate api
-#  application, maybe
